f2373ac34913169fdcea2cf04cf70e29bddae8c7
Makdoom Shaikh <makshaikh99@gmail.com>
2020-07-11 21:35:35 +0530

    Updates

---
+++ precache-manifest.1ce780605da3ea2a5741f68c086fb523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077de40e32078f0852bcd1d554fa1c78",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "856d40f93d183ab20c86",
+    "url": "/todolist/static/css/main.b72073c9.chunk.css"
+  },
+  {
+    "revision": "36300c17048ecc20020d",
+    "url": "/todolist/static/js/2.ebb88e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.ebb88e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856d40f93d183ab20c86",
+    "url": "/todolist/static/js/main.d3ef7850.chunk.js"
+  },
+  {
+    "revision": "b10e54e3a5eba11cb983",
+    "url": "/todolist/static/js/runtime-main.12bf549d.js"
+  }
+]);
